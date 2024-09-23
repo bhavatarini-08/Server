d026cddebfee40be51c416f540fdf12d5652cb44
bhavatarini-08 <bhavatariniselvan@gmail.com>
2024-09-23 12:26:00 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const app=express()
+
+require('dotenv').config()
+const port=process.env.PORT || 7778
+
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.json({message:"Welcome"})
+})
+
+app.listen(port,()=>{
+    console.log(`Server running in : ${port}`)
+})
