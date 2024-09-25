2a0353dfe84e7d3dd004e2802ae3b105279d892c
bhavatarini-08 <bhavatariniselvan@gmail.com>
2024-09-25 15:14:04 +0530

    Updated Usermodel

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,15 +1,33 @@
-const mongoose=require('mongoose')
 
-const projectSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
+const mongoose = require('mongoose')
+
+// "title": "title 1",
+// "coverimg": "https://loremflickr.com/640/480/cats",
+// "desc": "desc 1",
+// "link": "https://loremflickr.com/640/480",
+
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
     },
-    desc:{
-        type:String,
-        required:true,
-    }
+    desc: {
+        type: String,
+        required: true
+    },
+    coverimg: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: false,
+    },
+   
+
+
+
 })
 
-const Projects= mongoose.model("Projects",projectSchema)
-module.exports=Projects;
+const Projects = mongoose.model("Projects", projectSchema)
+module.exports = Projects;
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const express= require('express')
-const app=express()
-const dbconn=require('./config/db')
-const Projects=require('./routes/projectRoute')
 require('dotenv').config()
-const port=process.env.PORT || 7778
-
-app.use('/projects',Projects)
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dbConn = require('./config/db')
+const Projects = require('./routes/projectRoute')
+const Users = require('./routes/userRoute')
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.json({message:"Welcome"})
+// const dotenv = require('dotenv')
+// dotenv.config()
+// const corsOptions = {
+//     origin: process.env.CORSURL
+// };
+app.use(cors())
+const port = process.env.PORT || 7778
+app.use('/projects', Projects)
+app.use('/users', Users)
+
+app.get('/', (req, res) => {
+    res.status(400).json("Welcome")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running in : ${port}`)
 })
